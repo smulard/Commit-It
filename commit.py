59587e3b93ce987bf580a1e598c2f780c316bab2
Sophie Mulard <sophiemulard@datamachines.io>
2021-12-22 21:56:15 -0500

    12/22/2021 21:56:15

--- commit.py
+++ commit.py
@@ -1,16 +1,38 @@
 
-import datetime
-import subprocess as cmd
+import csv
+import requests
+import json
+from github import Github
+import uuid
+from git import Repo
+import subprocess
+import subprocess
 
-current_time = datetime.datetime.now()
+
+# create file if it doesn't exit and adds a new uuid to the file
+with open("changes.txt", "a+") as f:
+    delta = uuid.uuid4().hex # new hash that changes status of the file
+    f.write(delta + "\n")
+
+# add untracked hash file
+repo = Repo(search_parent_directories=True)
+print(repo) # path of git repo 
+adding = subprocess.run(["git", "add", "changes.txt"])
+
+print (adding)
+
+# access creds for github: username, github token, repo url
+with open ('creds.txt') as f:
+    reader = csv.reader(f, delimiter=",")
+    for i, row in enumerate(reader):
+        username = row[0]
+        token = row[1]
+        repo_url = row[2]
+        gh_repo = row[3]
+
+#access to github
+g = Github(token)
 
 
-# Commit and push up latest changes
-cp = cmd.run("git add .", check=True, shell=True)
-print(cp)
 
-cp = cmd.run(f"git commit -m 'adding {current_time}'", check=True, shell=True)
-print(cp)
 
-cp = cmd.run("git push -u origin develop -f", check=True, shell=True)
-print(cp)
